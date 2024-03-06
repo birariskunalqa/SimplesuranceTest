d9851bd9df3189ba83f9b5c3bd49d1cca1b6f423
Kunal Biraris <birariskunalqa@gmail.com>
2024-03-06 12:35:16 +0100

    added assert messages in test class

--- src/test/java/test_cases/CreateNewInsurancePolicy.java
+++ src/test/java/test_cases/CreateNewInsurancePolicy.java
@@ -10,45 +10,47 @@ import page_objects.PaymentDetailsPage;
 import page_objects.PaymentProcedurePage;
 import test_base.BaseTest;
 
+import static org.testng.AssertJUnit.assertTrue;
+
 public class CreateNewInsurancePolicy extends BaseTest{
 
     @Test
     public void verifyCreateNewInsurancePolicyByFillingAllMandatoryData(){
 
+        //Login
         LoginPage loginPage = new LoginPage(driver);
-
-        loginPage.clickLanguageMenuIcon();
-        loginPage.clickEnglishLanguageMenuItem();
+        assertTrue("unable to click on language menu icon", loginPage.clickLanguageMenuIcon());
+        assertTrue("unable to click on 'English' menu item", loginPage.clickEnglishLanguageMenuItem());
 
         String email = p.getProperty("email");
-        System.out.println(email);
-        loginPage.sendKeysToEmail(email);
+        assertTrue("unable to enter text in 'E-Mail' field", loginPage.sendKeysToEmail(email));
 
         String password = p.getProperty("password");
-        loginPage.sendKeysToPassword(password);
-        loginPage.clickNextButton();
+        assertTrue("unable to enter text in 'Password' field", loginPage.sendKeysToPassword(password));
+        assertTrue("unable to click on 'NEXT' button", loginPage.clickNextButton());
 
+        //
         HomePage homePage = new HomePage(driver);
-        homePage.clickNewLink();
+        assertTrue("unable to click on 'NEW' link",homePage.clickNewLink());
 
         ProductContainerPage productContainerPage = new ProductContainerPage(driver);
-        productContainerPage.clickCountryDropdown();
-        productContainerPage.clickCountryGermany();
-        productContainerPage.clickProductNameDropdown();
-        productContainerPage.clickNokiav1Allianzglobal();
-        productContainerPage.clickTariffDropdown();
-        productContainerPage.clickTariffValue();
-        productContainerPage.clickInsuranceCategoryDropDown();
-        productContainerPage.clickInsuranceCategoryValue();
-        productContainerPage.clickPeriodDropdown();
-        productContainerPage.clickPeriodValue();
-        productContainerPage.clickPaymentTypeDropdown();
-        productContainerPage.clickPaymentTypeValue();
-        productContainerPage.clickClassDropdown();
-        productContainerPage.clickClassDropdownValue();
+        assertTrue("unable to click on 'Country' dropdown", productContainerPage.clickCountryDropdown());
+        assertTrue("unable to click on 'Germany' option in 'Country' dropdown", productContainerPage.clickCountryGermany());
+        assertTrue("unable to click on 'Product Name' dropdown", productContainerPage.clickProductNameDropdown());
+        assertTrue("unable to click on 'Nokia_v1Allianz_global' option in 'Product Name' dropdown", productContainerPage.clickNokiav1Allianzglobal());
+        assertTrue("unable to click on 'Tariff' dropdown", productContainerPage.clickTariffDropdown());
+        assertTrue("unable to click on '1-34-Nokia_v1Allianz_global-D-EUR-v1-Accidental_damag' option in 'Tariff' dropdown", productContainerPage.clickTariffValue());
+        assertTrue("unable to click on 'Insurance category' dropdown", productContainerPage.clickInsuranceCategoryDropDown());
+        assertTrue("unable to click on 'CATEGORY_SMARTPHONE' option in 'Insurance category' dropdown", productContainerPage.clickInsuranceCategoryValue());
+        assertTrue("unable to click on 'Period' dropdown", productContainerPage.clickPeriodDropdown());
+        assertTrue("unable to click on '12 month(s)' option in 'Period' dropdown", productContainerPage.clickPeriodValue());
+        assertTrue("unable to click on 'Payment type' dropdown", productContainerPage.clickPaymentTypeDropdown());
+        assertTrue("unable to click on 'One-time' option in 'Payment type' dropdown", productContainerPage.clickPaymentTypeValue());
+        assertTrue("unable to click on 'Class' dropdown", productContainerPage.clickClassDropdown());
+        assertTrue("unable to click on 'Nokia 3/500 - EUR 500.00' option in 'Class' dropdown", productContainerPage.clickClassDropdownValue());
         String serialNumber = "1234567890";
-        productContainerPage.sendKeysToSerialNumberTextBox(serialNumber);
-        productContainerPage.clickNextButton();
+        assertTrue("unable to enter text in 'Serial number' field",productContainerPage.sendKeysToSerialNumberTextBox(serialNumber));
+        assertTrue("unable to click on 'NEXT' button", productContainerPage.clickNextButton());
 
         CustomerContainerPage customerContainerPage = new CustomerContainerPage(driver);
 
